7796a03a1e4c7ae710abfe178fbcd7cda3f56f1f
Slowth-KIM <rt.slowth@gmail.com>
2021-06-10 13:17:43 +0900

    초기 프로젝트 생성

---
+++ src/test/java/tokenrest/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package tokenrest.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
